5f502fe4dfdcb6c7240d0a9e54e234e21d36759f
igor.burdunin <igor.burdunin@thetoadman.com>
2022-09-19 23:49:09 +0300

    Update shader compiling script debug logs

--- compile_shaders.py
+++ compile_shaders.py
@@ -1,10 +1,12 @@
 import subprocess as sp
 import os
+import time
 
 for sub_dir, dirs, files in os.walk("shaders/src"):
     for file in files:
         filepath = sub_dir + os.sep + file
 
         if filepath.endswith(".frag") or filepath.endswith(".vert"):
-            print("Compiling", file)
+            start_time = time.time()
             sp.run(["glslc", filepath, "-o", "shaders/spv/" + file + ".spv"])
+            print("Compiled ", file, " for ", (time.time() - start_time), "seconds")
